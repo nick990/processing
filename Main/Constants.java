8d00cfbb22183cf3b00899ccaa80938d51496822
nicolo <nicolo.festa90@gmail.com>
2021-05-03 18:43:39 +0200

    Create main project

--- Main/Constants.java
+++ Main/Constants.java
@@ -8,16 +8,18 @@ public class Constants{
         Constants.app=app;
         Constants.TILES_INDEXES_VALID=new ArrayList<Integer>();
     }
-    public static int WIDTH=1440;
-    public static int HEIGHT=3040;
     public static double TILE_PADDING_RATIO=1.0/5.0;
-    public static int COLS=8;
-    public static int ROWS=3;
-    public static int TILE_SIZE=300;    
+    public static int TILE_SIZE=500;
+    public static int COLS=5;
+    public static int ROWS=5;
     public static int CORNER=3;
     public static int LEVELS=2;
     public static Double SPLIT_RATE = 0.46;
     public static int BLUE=255/2;
     public static int SMOOTH=8;
+    public static int WIDTH=200;
+    public static int HEIGHT=200;
+    public static int SPLIT_THRESHOLD=40;
+    public static int MIN_TILE_SIZE=20;
 
 }
